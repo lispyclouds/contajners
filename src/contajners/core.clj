7f5f222d64fa0e26dc45fcf71a89d13d98881472
Rahul De <rahul080327@gmail.com>
2021-08-02 19:58:50 +0200

    initial client code, WIP

--- src/contajners/core.clj
+++ src/contajners/core.clj
@@ -1,1 +1,92 @@
-(ns contajners.core)
+(ns contajners.core
+  (:require
+    [clojure.data.json :as json]
+    [contajners.requests :as req]
+    [unixsocket-http.core :as http]))
+
+(defn categories
+  [engine version]
+  (->> (req/load-api engine version)
+       (keys)
+       (remove #(= "contajners" (namespace %)))))
+
+(defn client
+  [{:keys [engine category conn version]}]
+  (let [api (req/load-api engine version)]
+    {:api     (-> api
+                  category
+                  (merge (select-keys api [:contajners/doc-url])))
+     :conn    (http/client (:uri conn) {:mode :recreate})
+     :version version}))
+
+(defn ops
+  [{:keys [api]}]
+  (keys api))
+
+(defn doc
+  [{:keys [version api]} op]
+  (some-> api
+          op
+          (select-keys [:summary])
+          (assoc :doc-url
+                 (format (:contajners/doc-url api)
+                         version
+                         (name op)))))
+
+(defn invoke
+  [{:keys [version conn api]} {:keys [op params as throw-exceptions throw-entire-message]}]
+  (let [operation      (op api)
+        request-params (reduce (partial req/gather-params params)
+                               {}
+                               (:params operation))
+        request {:client           conn
+                 :method           (:method operation)
+                 :url              (str "/"
+                                        version
+                                        (-> operation
+                                            :path
+                                            (req/interpolate-path (:path request-params))))
+                 :headers          (:headers request-params)
+                 :query-params     (:query request-params)
+                 :body             (:body params)
+                 :as               (or as :string)
+                 :throw-exceptions throw-exceptions
+                 :throw-entire-message? throw-entire-message}]
+    (-> request
+        (req/maybe-serialize-body)
+        (http/request)
+        :body
+        (json/read-str :key-fn keyword))))
+
+(comment
+  (categories :podman "v3.2.3")
+
+  (categories :docker "v1.41")
+
+  (def client
+    (client {:engine   :podman
+             :version  "v3.2.3"
+             :category :libpod/containers
+             :conn     {:uri "http://localhost:8080"}}))
+
+  (def d-client
+    (client {:engine   :docker
+             :version  "v1.41"
+             :category :containers
+             :conn     {:uri "unix:///var/run/docker.sock"}}))
+
+  (ops client)
+
+  (ops d-client)
+
+  (doc client :ContainerCreateLibpod)
+
+  (doc d-client :ContainerCreate)
+
+  (invoke client
+          {:op     :ContainerListLibpod
+           :params {:all true}})
+
+  (invoke d-client
+          {:op     :ContainerList
+           :params {:all true}}))
--- src/contajners/requests.clj
+++ src/contajners/requests.clj
@@ -1,9 +1,76 @@
 (ns contajners.requests
-  (:require [unixsocket-http.core :as http]))
+  (:require
+    [clojure.edn :as edn]
+    [clojure.java.io :as io]
+    [clojure.data.json :as json]
+    [clojure.string :as s]
+    [unixsocket-http.core :as http])
+  (:import
+    [java.io PushbackReader]
+    [java.util.regex Pattern]))
+
+(defn load-api
+  [engine version]
+  (if-let [config (io/resource
+                    (format "contajners/%s/%s.edn"
+                            (name engine)
+                            version))]
+    (-> config
+        (io/reader)
+        (PushbackReader.)
+        (edn/read))
+    (throw (IllegalArgumentException. "Cannot load api, the engine, version combo may not be supported."))))
+
+(defn gather-params
+  [supplied-params request-params {:keys [name in]}]
+  (let [param (keyword name)]
+    (if-not (contains? supplied-params param)
+      request-params
+      (update-in request-params [(keyword in)] assoc param (param supplied-params)))))
+
+(defn maybe-serialize-body
+  "If the body is a map, convert it to JSON and attach the correct headers."
+  [{:keys [body] :as request}]
+  (if (map? body)
+    (-> request
+        (assoc-in [:headers "content-type"] "application/json")
+        (update :body json/write-str))
+    request))
+
+(defn interpolate-path
+  "Replaces all occurrences of {k1}, {k2} ... with the value map provided.
+  Example:
+  given a/path/{id}/on/{not-this}/root/{id} and {:id hello}
+  results in: a/path/hello/{not-this}/root/hello."
+  [path value-map]
+  (let [[param value] (first value-map)]
+    (if (nil? param)
+      path
+      (recur (s/replace path
+                        (re-pattern (format "\\{([%s].*?)\\}"
+                                            (-> param
+                                                name
+                                                Pattern/quote)))
+                        (str value))
+             (dissoc value-map param)))))
 
 (comment
+  (load-api :podman "v3.2.3")
+
   (def client (http/client "tcp://localhost:8080"))
 
   (http/get client "/v1.40/_ping")
 
-  (http/get client "/containers/json"))
+  (http/get client "/containers/json")
+
+  (reduce (partial gather-params {:a 42 :b 64 :c 44})
+          {}
+          [{:name "a" :in :path}
+           {:name "b" :in :query}
+           {:name "c" :in :query}])
+
+  (maybe-serialize-body {:body {:a 42}})
+
+  (maybe-serialize-body {:body 42})
+
+  (interpolate-path "/a/{w}/b/{x}/{y}" {:x 41 :y 42 :z 43}))
