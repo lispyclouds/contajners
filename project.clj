5740300c6854ba91514362b6c2391d1a3fea96d5
Rahul De <rahul080327@gmail.com>
2021-08-03 13:44:34 +0200

    add lein for easier publish

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject org.clojars.lispyclouds/contajners "0.0.1"
+  :author       "Rahul De <rahul@mailbox.org>"
+  :url          "https://github.com/lispyclouds/contajners"
+  :description  "An idiomatic, data-driven, REPL friendly clojure client for OCI container engines"
+  :license      {:name "MIT"
+                 :url  "https://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/data.json "2.4.0"]
+                 [unixsocket-http/unixsocket-http "1.0.8"]])
