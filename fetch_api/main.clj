ad4ffbb5f1972de9815c00f4c6d59b70101ec0a5
Rahul De <rahul080327@gmail.com>
2023-04-07 08:53:14 +0100

    [housekeeping] Podman v4.4.4, deps

--- fetch_api/main.clj
+++ fetch_api/main.clj
@@ -61,7 +61,8 @@
                        "v4.4.0"
                        "v4.4.1"
                        "v4.4.2"
-                       "v4.4.3"]}})
+                       "v4.4.3"
+                       "v4.4.4"]}})
 
 (def resource-path "resources/contajners")
 
