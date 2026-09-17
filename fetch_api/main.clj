292da8a5a267401f98051ab60213cfe0d490b2b2
Rahul De <rahul080327@gmail.com>
2026-09-17 08:23:37 +0100

    [housekeeping] podman 6.1.2, deps

--- fetch_api/main.clj
+++ fetch_api/main.clj
@@ -128,7 +128,8 @@
                         "v6.0.1"
                         "v6.0.2"
                         "v6.1.0"
-                        "v6.1.1"]}}})
+                        "v6.1.1"
+                        "v6.1.2"]}}})
 
 (def resource-path "resources/contajners")
 
